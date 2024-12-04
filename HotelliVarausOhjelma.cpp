87f3aeab40887b1ca0297d1f512b0389094368d0
Joonas Akers <joonas.akers@tuni.fi>
2024-12-04 15:27:49 +0200

    v1.2 comments added, options added

--- HotelliVarausOhjelma.cpp
+++ HotelliVarausOhjelma.cpp
@@ -12,9 +12,9 @@
 #include<random>
 using namespace std;
 
-
 //prototyypit
 int randn(int minimi, int maksimi);
+int hintalasku(int tyyppi, int yöt, int alennus);
 
 
 int main() {
@@ -25,29 +25,27 @@ int main() {
     fstream infile;
     string line;
     vector<string> lines;
-    int huoneMäärä;
-    int huoneMäärä1hh;
-    int huoneMäärä2hh;
-    //random_device rd;
-    //mt19937 gen(rd());
-    int randNum = 0;
-    vector<bool> varausNumerot(100000, 0);
-    /*for (int i = 0; i < 100000; i++) {
-        varausNumerot.push_back(0);
-        cout << varausNumerot.size() << endl;
-    }*/
-    string varausKonfirm;
-    string uusiVaraus;
+    string vaihtoehto; //mitä haluat tehdä ohjelmalla
+    int huoneMäärä; //hotellin honeiden kokonaismäärä
+    int huoneMäärä1hh; //jäljellä olevia yhden henkilön huoneet
+    int huoneMäärä2hh; //jäljellä olevia kahden henkilön huoneet
+    int randNum = 0; //satunnaisluvun hetkellinen tallennus paikka
+    vector<bool> varausNumerot(100000, 0); //lista käytetyistä varausnumeroista
+    string varausKonfirm; //varauksen varmistus onko oikein vai ei
+    string uusiVaraus; //vastaus kysymykseen halutko varata toinen huone
+    string off; //haluatko sulje ohjelma
 
+    //varauksen profiili
     struct varaus {
-        string nimi;
-        int huoneNumero;
-        int huoneTyyppi;
-        int alennus;
-        int yöt;
-        int varausNumero;
-        int hinta;
+        string nimi; //varaajan nimi
+        int huoneNumero; //varatun huoneen numero
+        int huoneTyyppi; //varatun huoneen tyyppi (1hh tai 2hh)
+        int alennus; //mahdollinen alennus
+        int yöt; //kuinka monta yötä on varattu
+        int varausNumero; //varauksen numero
+        int hinta; //varauksen kokonais hinta
 
+        //tulostaa varauksen tiedot
         void näytäVaraus() {
             cout << "Nimi: " << nimi << endl;
             cout << "Huone Tyyppi: " << huoneTyyppi << endl;
@@ -58,7 +56,8 @@ int main() {
             cout << "Varausnumero: " << varausNumero << endl;
         }
     };
-    vector<varaus> varaukset;
+
+    vector<varaus> varaukset; //lista kaikista varatuista huoneista
 
     //Hae olemassa olevia hotellivaraukset (jos on)
     infile.open("varaukset.txt", fstream::in);
@@ -70,6 +69,7 @@ int main() {
         infile.close();
     }
     else {
+        //tekee uusi hotelli jos ei löydä tallennettu hotelli
         cout << "Ei olemassa olevia varauksia, luodaan hotelli..." << endl;
         randNum = randn(20, 131);
         huoneMäärä = randNum * 2;
@@ -78,91 +78,93 @@ int main() {
         cout << "Huoneiden määrä: " << huoneMäärä << endl << "1hh: " << huoneMäärä1hh << endl << "2hh: " << huoneMäärä2hh << endl;
     }
 
-    vector<bool> varattu1hh(huoneMäärä1hh + 1, 0); //varattu1hh[300] = 0;
-    vector<bool> varattu2hh(huoneMäärä2hh + 1, 0); //varattu2hh[300] = 0; 
+    //vectorit jolla tarkistetaan onko tietty huone varattu
+    vector<bool> varattu1hh(huoneMäärä1hh + 1, 0); 
+    vector<bool> varattu2hh(huoneMäärä2hh + 1, 0);  
 
-
-    //Varaa huone
-    for (int j = 0; j < 1; j++) {
-        varaus tempVaraus;
-        cout << "Mikä teidän nimenne on? ";
-        getline(cin, tempVaraus.nimi);
-        cout << "Haluaisitko yhden hengen huone (1) tai kahden hengen huone (2)? ";
-        cin >> tempVaraus.huoneTyyppi;
-        for (int i = 0; i < 1; i++) {
-            cout << "Mikä huonenumero haluaisitte? ";
-            cin >> tempVaraus.huoneNumero;
-            if (tempVaraus.huoneTyyppi == 1 && varattu1hh[tempVaraus.huoneNumero] == 1) {
-                cout << "Huone on Varattu, valitse toinen huone";
-                i--;
-            }
-            if (tempVaraus.huoneTyyppi == 2 && varattu2hh[tempVaraus.huoneNumero] == 1) {
-                cout << "Huone on Varattu, valitse toinen huone";
-                i--;
-            }
-        }
-        randNum = randn(0, 3);
-        tempVaraus.alennus = randNum * 10;
-        cout << "Kuinka monta yötä olette täällä? ";
-        cin >> tempVaraus.yöt;
-        for (int i = 0; i < 1; i++) {
-            randNum = randn(10000, 100000);
-            if (varausNumerot[randNum] == true) {
-                i--;
-            }
-        }
-        tempVaraus.varausNumero = randNum;
-        if (tempVaraus.huoneTyyppi == 1) {
-            tempVaraus.hinta = (100.00 * tempVaraus.yöt) - (100.00 * tempVaraus.yöt / 100.00) * tempVaraus.alennus;
-        }
-        if (tempVaraus.huoneTyyppi == 2) {
-            tempVaraus.hinta = (150.00 * tempVaraus.yöt) - (150.00 * tempVaraus.yöt / 100.00) * tempVaraus.alennus;
-        }
-        tempVaraus.näytäVaraus();
-        cout << "Konfirmoi varaus (y/n)? ";
-        cin >> varausKonfirm;
-        if (varausKonfirm == "y" || varausKonfirm == "Y" || varausKonfirm == "yes" || varausKonfirm == "Yes" || varausKonfirm == "YES" || varausKonfirm == "k" || varausKonfirm == "K" || varausKonfirm == "kyllä" || varausKonfirm == "Kyllä" || varausKonfirm == "KYLLÄ" || varausKonfirm == "kylla" || varausKonfirm == "Kylla" || varausKonfirm == "KYLLA") {
-            varaukset.push_back(tempVaraus);
-            if (tempVaraus.huoneTyyppi == 1) {
-                varattu1hh[tempVaraus.huoneNumero] = 1;
-                huoneMäärä1hh--;
-            }
-            if (tempVaraus.huoneTyyppi == 2) {
-                varattu2hh[tempVaraus.huoneNumero] = 1;
-                huoneMäärä2hh--;
-            }
-        }
-        cout << "Tee uusi varaus (y/n)? ";
-        cin >> uusiVaraus;
+    for (int k = 0; k < 1; k++) {
+        cout << "Mitä haluat tehdä? " << endl << "V = Varaa Huone. " << endl << "S = Sulje Ohjelma. ";
+        cin >> vaihtoehto;
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        if (uusiVaraus == "y" || uusiVaraus == "Y" || uusiVaraus == "yes" || uusiVaraus == "Yes" || uusiVaraus == "YES" || uusiVaraus == "k" || uusiVaraus == "K" || uusiVaraus == "kyllä" || uusiVaraus == "Kyllä" || uusiVaraus == "KYLLÄ" || uusiVaraus == "kylla" || uusiVaraus == "Kylla" || uusiVaraus == "KYLLA") {
-            j--;
+
+        if (vaihtoehto == "v" || vaihtoehto == "V" || vaihtoehto == "varaa" || vaihtoehto == "Varaa" || vaihtoehto == "VARAA" || vaihtoehto == "varaa huone" || vaihtoehto == "Varaa huone" || vaihtoehto == "Varaa Huone" || vaihtoehto == "VARAA HUONE") {
+            //Varaa huone
+            for (int j = 0; j < 1; j++) {
+                varaus tempVaraus;//hetkellinen varaus
+
+                cout << "Mikä teidän nimenne on? ";
+                getline(cin, tempVaraus.nimi);
+
+                cout << "Haluaisitko yhden hengen huone (1) tai kahden hengen huone (2)? ";
+                cin >> tempVaraus.huoneTyyppi;
+
+                for (int i = 0; i < 1; i++) {
+                    cout << "Mikä huonenumero haluaisitte? ";
+                    cin >> tempVaraus.huoneNumero;
+                    if (tempVaraus.huoneTyyppi == 1 && varattu1hh[tempVaraus.huoneNumero] == 1) {
+                        cout << "Huone on Varattu, valitse toinen huone";
+                        i--;
+                    }
+                    if (tempVaraus.huoneTyyppi == 2 && varattu2hh[tempVaraus.huoneNumero] == 1) {
+                        cout << "Huone on Varattu, valitse toinen huone";
+                        i--;
+                    }
+                }
+
+                //arpoo alennus
+                randNum = randn(0, 2);
+                tempVaraus.alennus = randNum * 10;
+
+                cout << "Kuinka monta yötä olette täällä? ";
+                cin >> tempVaraus.yöt;
+
+                //arpoo varausnumero
+                for (int i = 0; i < 1; i++) {
+                    randNum = randn(10000, 100000);
+                    if (varausNumerot[randNum] == true) {
+                        i--;
+                    }
+                }
+                tempVaraus.varausNumero = randNum;
+
+                //laskee hinta
+                tempVaraus.hinta = hintalasku(tempVaraus.huoneTyyppi, tempVaraus.yöt, tempVaraus.alennus);
+
+                //varmista onko haluatko tallentaa varaus (vimeinen hetki tarkistaa onko kirjoitettu oikein
+                tempVaraus.näytäVaraus();
+                cout << "Konfirmoi varaus (y/n)? ";
+                cin >> varausKonfirm;
+                if (varausKonfirm == "y" || varausKonfirm == "Y" || varausKonfirm == "yes" || varausKonfirm == "Yes" || varausKonfirm == "YES" || varausKonfirm == "k" || varausKonfirm == "K" || varausKonfirm == "kyllä" || varausKonfirm == "Kyllä" || varausKonfirm == "KYLLÄ" || varausKonfirm == "kylla" || varausKonfirm == "Kylla" || varausKonfirm == "KYLLA") {
+                    varaukset.push_back(tempVaraus);
+                    if (tempVaraus.huoneTyyppi == 1) {
+                        varattu1hh[tempVaraus.huoneNumero] = 1;
+                        huoneMäärä1hh--;
+                    }
+                    if (tempVaraus.huoneTyyppi == 2) {
+                        varattu2hh[tempVaraus.huoneNumero] = 1;
+                        huoneMäärä2hh--;
+                    }
+                }
+
+                //haluatko tehdä toinen varaus
+                cout << "Tee uusi varaus (y/n)? ";
+                cin >> uusiVaraus;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                if (uusiVaraus == "y" || uusiVaraus == "Y" || uusiVaraus == "yes" || uusiVaraus == "Yes" || uusiVaraus == "YES" || uusiVaraus == "k" || uusiVaraus == "K" || uusiVaraus == "kyllä" || uusiVaraus == "Kyllä" || uusiVaraus == "KYLLÄ" || uusiVaraus == "kylla" || uusiVaraus == "Kylla" || uusiVaraus == "KYLLA") {
+                    j--;
+                }
+                k--;
+            }
         }
-        /*for (int i = 0; i < varattu1hh.size(); i++) {
-            cout << varattu1hh[i] << endl;
+        if (vaihtoehto == "s" || vaihtoehto == "S" || vaihtoehto == "sulje" || vaihtoehto == "Sulje" || vaihtoehto == "SULJE" || vaihtoehto == "sulje ohjelma" || vaihtoehto == "Sulje ohjelma" || vaihtoehto == "Sulje Ohjelma" || vaihtoehto == "SULJE OHJELMA") {
+            cout << "Sulje hotelli managementti Ohjelma (y/n)? ";
+            cin >> off;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            if (off == "n" || off == "N" || off == "no" || off == "No" || off == "NO" || off == "e" || off == "E" || off == "ei" || off == "Ei" || off == "EI") {
+                k--;
+            }
         }
-        for (int i = 0; i < varattu2hh.size(); i++) {
-            cout << varattu2hh[i] << endl;
-        }*/
     }
-    //struct varaus varaus = {"nimi", 16, 2, 1, 5, 11111};
-
-
-
-    //debug
-    /*for (int i = 0; i < 1000; i++) {
-        randnum = randn();
-        huoneMäärä = randnum;
-        cout << huoneMäärä * 2 << " ";
-    }*/
-
-
-    /*funktio
-    for (int i = 0; i < lines.size(); i++) {
-        for (int j = 0; j < lines[i].length(); j++)
-            lines[i][j] = toupper(lines[i][j]);
-    }*/
-
 
 
 
@@ -192,4 +194,18 @@ int randn(int minimi, int maksimi) {
     uniform_int_distribution<> dis(minimi, maksimi);
 
     return dis(gen);
+}
+
+// laskee hinnat
+int hintalasku(int tyyppi, int yöt, int alennus) {
+    int hinta;
+
+    if (tyyppi == 1) {
+        hinta = (100.00 * yöt) - (100.00 * yöt / 100.00) * alennus;
+    }
+    if (tyyppi == 2) {
+        hinta = (150.00 * yöt) - (150.00 * yöt / 100.00) * alennus;
+    }
+
+    return hinta;
 }
